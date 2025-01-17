d4cffbb29e9b5786da99f78b8275c853e57568b4
Chris Koorzen <fckoorzen@gmail.com>
2025-01-17 19:22:45 +0200

    Implement basic DB connection logic

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
+)
 
 func main() {
 	fmt.Println("Hello, Subscription Service!")
 
 	// connect to database
+	db := initDB()
+	fmt.Println(db)
 
 	// create sessions
 
@@ -18,4 +29,53 @@ func main() {
 	// set up mail
 
 	// listen for connections
+}
+
+func initDB() *sql.DB {
+
+	conn := connectToDB()
+	if conn == nil {
+		log.Panic("Failed to connect to database")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	counts := 0 // number of attempts to connect to database
+
+	dsn := os.Getenv("DSN") // Database Source Name
+
+	for {
+		connection, err := openDB(dsn)
+		if err != nil {
+			log.Println("Database not yet ready...")
+		} else {
+			log.Println("Connected to database")
+			return connection
+		}
+
+		// Retry every 1 second
+		if counts > 5 {
+			return nil
+		}
+		counts++
+		time.Sleep(1 * time.Second)
+		continue
+	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	// Open connection to database
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	// Check if connection is valid
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
